0e58079ef8dac6e2e190e6426ab41790d5abcd50
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-07-25 13:33:19 -0300

    new update

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,12 +1,16 @@
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from todoapp.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-update_at')
+    
+    completed_tasks = Task.objects.filter(is_completed=True)
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
     }
     return render(request, 'home.html', context)
+
--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.addTask, name='addTask')
+    path('addTask/', views.addTask, name='addTask'),
+    # Mark as done
+    path('mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done'),
+    
+    # Mark as Undone
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone'),
+    
+    # Edit/Delete Feature
+    path('deleteTask/<int:pk>', views.deleteTask, name='deleteTask'),
+    path('edit_task/<int:pk>', views.edit_task, name='edit_task'),
+    
 ]
